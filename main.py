aa589f4b903bddebb08ddace6760ebf554318011
jordankopp <jorkopp@gmail.com>
2026-01-17 12:18:02 -0800

    fixed main and daily_alert to only send new jobs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.scraper import Scraper
 from src.company import Company
-from src.emailer import send_email
+from src.notification_generator import send_email
+from src.database.db import NotifiedDatabase
 
 def main():
     all_jobs = set()
@@ -13,12 +14,22 @@ def main():
     if not all_jobs:
         return
 
-    body = "\n\n".join(job.description() for job in all_jobs)
+    previously_notified = NotifiedDatabase.load_previously_notified_jobs()
+
+    new_jobs = all_jobs - previously_notified
+
+    if not new_jobs:
+        print("No new jobs found.")
+        return
+
+    body = "\n\n".join(job.description() for job in new_jobs)
 
     send_email(
-        subject="New Job Matches Found",
+        subject=f"{len(new_jobs)} New Job Matches Found",
         body=body
     )
+
+    NotifiedDatabase.register_jobs_as_notified(new_jobs)
 
 if __name__ == "__main__":
     main()
