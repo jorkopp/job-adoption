ca6967fadc0838d9a0810811ee78357fe3873c80
jordankopp <jorkopp@gmail.com>
2026-01-16 21:54:49 -0800

    updated github actions workflow

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
-from src.notification_generator import NotificationGenerator
+from src.scraper import Scraper
+from src.company import Company
+from src.emailer import send_email
 
-if __name__ == '__main__':
-    NotificationGenerator.run()
+def main():
+    all_jobs = set()
+
+    for company in Company.all_companies():
+        jobs = Scraper.scrape_jobs(company)
+        jobs = Scraper.filter_applicable_jobs(jobs)
+        all_jobs.update(jobs)
+
+    if not all_jobs:
+        return
+
+    body = "\n\n".join(job.description() for job in all_jobs)
+
+    send_email(
+        subject="New Job Matches Found",
+        body=body
+    )
+
+if __name__ == "__main__":
+    main()
